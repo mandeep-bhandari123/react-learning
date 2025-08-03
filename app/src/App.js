47dc006d1d395bb150793a355fa6dfc415e8317e
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-08-03 16:36:11 +0545

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+let name = "Mandeep"
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>about</li>
+      <li>content</li>
+    </nav>
+    <div>
+      <h1>Hello {name}</h1>
+    </div>
+    </>
+
+  );
+}
+
+export default App;
